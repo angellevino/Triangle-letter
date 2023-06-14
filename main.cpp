7f6eb971160b554cb27bb1a8343915d4766b5c63
Anggelia Levino <104257044+angellevino@users.noreply.github.com>
2023-06-14 18:23:11 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// Given a string than make a triangle letter from it 
+// example:ABC
+//     A
+//   A   B
+// A   B   C
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,19 +19,20 @@ int main (){
     size_t n = letter.length();
     vector<vector<char>> pyramid(n, vector<char>(2*n-1,' '));
 
-    // insert elemet
+    // insert element
     for (size_t i{0} ; i < n ; ++i){
         for (size_t j = 0; j < i + 1; ++j) {
             pyramid[i][n - 1 - i + 2 * j] = letter[j];
         }
     }
         
-        // display pyramid
+    // display pyramid
         
-        for (const auto& row : pyramid) {
-            for (const auto& elem : row) {
-                cout << elem << " ";
-            }
-            cout << endl;
+    for (const auto& row : pyramid) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
         }
+    cout << endl;
+        }
+    
     }
